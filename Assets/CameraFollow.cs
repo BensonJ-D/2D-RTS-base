17b31b48464865be4f37aa13d14423e75518c394
James Benson <pheonixkatana@gmail.com>
2020-11-18 13:03:22 +0000

    No Comments

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] 
+    private CinemachineVirtualCamera camera;
+    private Camera _mainCam;
+    
+    [SerializeField] 
+    private float minFov = 1f;
+    [SerializeField] 
+    private float maxFov = 15f;
+    public float sensitivity = 10f;
+    // Start is called before the first frame update
+    private void Start()
+    {    
+        _mainCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var target = _mainCam.ScreenToWorldPoint(Input.mousePosition);
+        target.z = transform.position.z;
+        
+        var newPos = _mainCam.WorldToViewportPoint(target);
+        newPos.x = Mathf.Clamp01(newPos.x);
+        newPos.y = Mathf.Clamp01(newPos.y);
+        transform.position = _mainCam.ViewportToWorldPoint(newPos);
+        
+        var fov = camera.m_Lens.OrthographicSize;
+        var fovChange = Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+        fov += fovChange;
+        fov = Mathf.Clamp(fov, minFov, maxFov);
+        camera.m_Lens.OrthographicSize = fov;
+    }
+}
--- Assets/Selection.cs
+++ Assets/Selection.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
 public class Selection : MonoBehaviour
 {
@@ -9,38 +11,74 @@ public class Selection : MonoBehaviour
 
     public GameObject selectorPrefab;
 
+    public float scaleFactor = 1f;
+
     private RectTransform _selectorTransform;
 
     private BoxCollider2D _collider;
 
-    // [System.NonSerialized]
-    public bool selected = false;
+    private Boolean _selectable;
+
+    
+    public Vector3 target;
+    
+    public bool selected;
+
+    private Camera cam;
     // Start is called before the first frame update
     void Start()
     {
         _selectorTransform = Instantiate(selectorPrefab, uiLayer.transform).GetComponent<RectTransform>();
         _selectorTransform.gameObject.SetActive(false);
         _collider = GetComponent<BoxCollider2D>();
+        cam = Camera.main;
+
+        target = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!_selectorTransform.gameObject.activeSelf && selected)
+        
+        if (!_selectorTransform.gameObject.activeSelf && selected && _selectable)
         {
             _selectorTransform.gameObject.SetActive(true);
-            _selectorTransform.position = Camera.main.WorldToScreenPoint(transform.position);
-            _selectorTransform.sizeDelta = _collider.size * 120;
+            _selectorTransform.position = cam.WorldToScreenPoint(transform.position);
+            _selectorTransform.sizeDelta = _collider.size * 120 * scaleFactor;
         }
         else if (_selectorTransform.gameObject.activeSelf && !selected)
         {
             _selectorTransform.gameObject.SetActive(false);
         }
-        else if (_selectorTransform.gameObject.activeSelf && selected)
+        else if (_selectorTransform.gameObject.activeSelf && selected && _selectable)
         {
-            _selectorTransform.position = Camera.main.WorldToScreenPoint(transform.position);
-            _selectorTransform.sizeDelta = _collider.size * 120;
+            _selectorTransform.position = cam.WorldToScreenPoint(transform.position);
+            _selectorTransform.sizeDelta = _collider.size * 120 * scaleFactor ;
         }
+
+        if (selected && Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("Right mouse button pressed.");
+            var newTarget = cam.ScreenToWorldPoint(Input.mousePosition);
+            target = new Vector2(newTarget.x, newTarget.y);
+        }
+
+        if (transform.position != target)
+        {
+            var step = 2f * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, target, step);
+        }
+    }
+
+    private void OnBecameInvisible()
+    {
+        _selectable = false;
+        _selectorTransform.gameObject.SetActive(false);
+    }
+
+    private void OnBecameVisible()
+    {
+        _selectable = true;
     }
 
     private void OnMouseDown()
